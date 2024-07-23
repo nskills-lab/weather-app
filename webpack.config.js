43295d1e5a5c02c5c92fc8e4380e47b265976530
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-07-23 14:09:23 -0500

    Add header section

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,6 +9,11 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/template.html',
+    }),
+],
   module: {
     rules: [
       {
