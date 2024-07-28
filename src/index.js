6eb712d2bb96de9417b4b0d153cdb6a49bd9c2cc
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-07-27 19:13:16 -0500

    Add logic to format city name and display

--- src/index.js
+++ src/index.js
@@ -3,24 +3,33 @@
  * 1. Calling API with the location name - DONE
  * 2. Parsing data - DONE
  * 3. Displaying in the correct places - DONE
+ * 4. Getting city - DONE
  * 
- * 4. Getting city:
- *  - select OR input text field?
- * 5. updating step 1 with dynamic input
+ * 5. Defalt page when a user lands on the screen - DONE
+ * 6. Add a way to toggle between F and C options
+ * 7. Improve desing:
+ * - Add icons for weather conditions
+ * 8. Optimize code: 
+ * - more modular code
+ * - reduce redundancy
+ * - see if anything can be improved 
+ *  - see if any of JS advanced can be used
+ * 9. Update to include webpack 
  */
 
 import { timelineWeatherClient } from "./api/client/timelineWeather.js"
 import { week } from "./utils/dateUtils.js"
 const searchForm = document.getElementById('search-options-form')
+const cityInput =  document.getElementById('location-name')
 
 const displayWeatherForLocation = async (e) => {
     e.preventDefault();
     const cityToSearch =  document.getElementById('search').value 
-    console.log(cityToSearch)
     if (cityToSearch.length === 0 ) return alert("City name can't be blank!")
     try {
-    const weather = await getWeatherForecastWeek({location: cityToSearch})
-    await renderWeatherData(weather)
+    const forecast = await getWeatherForecastWeek({location: cityToSearch})
+    console.log(forecast)
+    await renderWeatherData({weather: forecast, location: cityToSearch})
     } catch (e){
         alert("Invalid request!")
     }
@@ -42,18 +51,29 @@ const getWeatherForecastWeek = async ({location, date1, date2} = {}) =>  {
     
 }
 
+const capitalize = (string)=> {
+    const strArr = string.trim().toLowerCase().split(' ')
+    const everyFirstLetterCapStr = strArr.map(
+        word => word[0].toUpperCase()+word.slice(1)
+    ).join(" ")
+    return everyFirstLetterCapStr
+}
 
-const renderWeatherData = async(weatherData) =>{
+
+const renderWeatherData = async({weather, location}) =>{
 
     document.querySelectorAll('[data-day]').forEach(dayCard => {
         const day = dayCard.dataset.day
-        const weatherDatum = weatherData.days[day]
+        const weatherDatum = weather.days[day]
         const formattedDate = weatherDatum.datetime.replaceAll('-', ',')
         const weekDay = week.getDate(formattedDate)
        
         if (day == 0){
+            
+            const cityFinalVal =  location ?? 'new york'
+            cityInput.innerText =  capitalize(cityFinalVal)
             const todayTempEl = dayCard.querySelector('[data-weekly-card-now]')
-            todayTempEl.innerText = `${weatherData.currentConditions.temp}°`
+            todayTempEl.innerText = `${weather.currentConditions.temp}°`
         } else {
             const dateEl = dayCard.querySelector('[data-weekly-card-date]')
             dateEl.innerText = `${weekDay}`
@@ -68,6 +88,10 @@ const renderWeatherData = async(weatherData) =>{
     });
 }
 
-
-
+const defaultDisplay = async()=>{
+    const weather = await getWeatherForecastWeek()
+    await renderWeatherData({weather: weather})
+    cityInput.innerText = 'New York'
+}
+await defaultDisplay()
 searchForm.addEventListener('submit', displayWeatherForLocation)
