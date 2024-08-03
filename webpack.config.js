6cbd7f22fc656dc2866225544dfa6b54788f45c4
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-08-03 13:01:37 -0500

    Add webpack built version

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
-  devtool: "inline-source-map",
+  entry: './src/index.js',
+  mode: 'development',
+  devtool: 'inline-source-map',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/template.html',
+      template: './src/template.html',
     }),
-],
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
-};
+}
