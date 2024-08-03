1d15051657fab55d7bd2e1f0a8a1a15135594ede
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-08-03 14:34:43 -0500

    Copy icon images to dist

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import AppController from './views/app.js'
 import './styles/main.css'
 import './styles/normalize.css'
-import './styles/icons/icon-clear-day.svg'
-import './styles/icons/icon-clear-night.svg'
-import './styles/icons/icon-cloudy.svg'
-import './styles/icons/icon-fog.svg'
-import './styles/icons/icon-partly-cloudy.svg'
-import './styles/icons/icon-partly-cloudy-night.svg'
-import './styles/icons/icon-rain.svg'
-import './styles/icons/icon-snow.svg'
-import './styles/icons/icon-wind.svg'
 
 await AppController.defaultDisplay()
 document.addEventListener('click', AppController.switchTimeScales)
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    assetModuleFilename: 'styles/icons/[hash][ext][query]',
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,10 +19,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
